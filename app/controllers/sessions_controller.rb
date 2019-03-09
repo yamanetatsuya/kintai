0ffac37a411fdcf29e67b4520e04a62311054108
Tatsuya Yamane <t.docdoc6@gmail.com>
2019-03-09 07:04:08 +0000

    log_in method added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
   def new
   end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+    # エラーメッセージ用のflashを入れる
+    flash.now[:danger] = 'メールアドレスとパスワードの情報が一致しませんでした。'
+    render 'new'
+    end
+  end
+  
+  def destroy
+  end
+  
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,6 @@
 module SessionsHelper
+  # 引数に渡されたユーザーでログインする
+  def log_in(user)
+    session[:user_id] = user.id
+  end
 end
